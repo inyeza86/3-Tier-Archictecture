2f17c9fee623164e9accf4b4194c8211fdc46d10
inyeza <inyeza1986@gmail.com>
2024-11-19 10:00:21 -0600

    security group added

---
+++ Security-groups.tf
@@ -0,0 +1,108 @@
+# Alb security group
+resource "aws_security_group" "alb-sg" {
+  description = "ALB security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Ec2 Instance connect endpoint SG
+resource "aws_security_group" "ec2-instance-connect-endpoint-sg" {
+  description = "Ec2 instance connect endpoint security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
+
+# Data migration server security group
+resource "aws_security_group" "data-migration-server-sg" {
+  description = "Data migration server security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2-instance-connect-endpoint-sg.id]
+  }
+
+}
+
+# webserver security group
+
+resource "aws_security_group" "web-server-sg" {
+  description = "Web server security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2-instance-connect-endpoint-sg.id]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS security group
+resource "aws_security_group" "rds-sg" {
+  description = "RDS security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web-server-sg.id]
+  }
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.data-migration-server-sg.id]
+  }
+}
