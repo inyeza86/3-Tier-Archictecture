5ee390126d43b7a3a09d6e986b770d87a62bd987
inyeza <inyeza1986@gmail.com>
2024-11-18 22:06:00 -0600

    remote updated

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "dynamic-webapp-on-aws"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "dynamic-webapp-on-aws"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "inyeza86"
   }
 }
