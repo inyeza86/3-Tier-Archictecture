60aeff2cc21781f4ff84fe005c8ee2063da6941a
inyeza <inyeza1986@gmail.com>
2024-11-18 22:39:15 -0600

    vpc updated

--- vpc.tf
+++ vpc.tf
@@ -114,10 +114,32 @@ resource "aws_subnet" "my_private_subnet4" {
     Name = "DEV-DB-Private-Subnet-AZ2"
   }
 }
+# create elastic ip for the NAT Gateway
+resource "aws_eip" "dev-nat-gateway" {
+  domain = "vpc"
+}
+
+# create nat-gateway
+resource "aws_nat_gateway" "dev-nat-gateway" {
+  allocation_id = aws_eip.dev-nat-gateway.id
+  subnet_id     = aws_subnet.my_public_subnet1.id
+
+  tags = {
+    Name = "DEV-NAT-GW"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.my_igw]
+}
 
 #create private route table
 resource "aws_route_table" "my_private_route_table" {
   vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.dev-nat-gateway.id
+  }
 
   tags = {
     Name = "DEV-Private-Route-Table"
@@ -136,3 +158,16 @@ resource "aws_route_table_association" "my_private_subnet2_association" {
   subnet_id      = aws_subnet.my_private_subnet2.id
   route_table_id = aws_route_table.my_private_route_table.id
 }
+# Associate private subnet3 with private route table
+
+resource "aws_route_table_association" "my_private_subnet3_association" {
+  subnet_id      = aws_subnet.my_private_subnet3.id
+  route_table_id = aws_route_table.my_private_route_table.id
+}
+# Associate private subnet4 with private route table
+resource "aws_route_table_association" "my_private_subnet4_association" {
+  subnet_id      = aws_subnet.my_private_subnet4.id
+  route_table_id = aws_route_table.my_private_route_table.id
+}
+
+
