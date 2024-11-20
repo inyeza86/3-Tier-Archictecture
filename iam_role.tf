85a3df2572700acb370e3ff16cd270afa03a8807
inyeza <inyeza1986@gmail.com>
2024-11-20 11:14:46 -0600

    iam role

---
+++ iam_role.tf
@@ -0,0 +1,21 @@
+# create iam_role with s3fullaccess permissions
+resource "aws_iam_role" "iam_role" {
+  name               = "s3-access-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
